3a5edeeaafbe3b5ca5077828f02a640b46e84171
sanjeevinisb <sanjeevini.xworkz@gmail.com>
2024-11-20 18:54:24 +0530

    Spring project initialization

---
+++ Glass/src/main/java/com/xworkz/glass/configuation/glass.java
@@ -0,0 +1,34 @@
+package com.xworkz.glass.configuation;
+
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class glass extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
+    public glass()
+    {
+        System.out.println("Created Glass");
+    }
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String[] urls={"/"};
+        System.out.println("configured urls for DispatcherService");
+        return urls;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        System.out.println("Enable static resource handling by server itself...");
+        configurer.enable();
+    }
+}
